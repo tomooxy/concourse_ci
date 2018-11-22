80556decd0004db0a198d1837062c06e311ba14d
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-11-22 17:34:04 +0900

    t

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 #export MAVEN_OPTS="-DsocksProxyHost=exaproxy.exa-corp.co.jp -DsocksProxyPort=8080"
 cd kantan_resource/kantanPro
 #mvn package spring-boot:repackage
-mvn package spring-boot:repackage -Dhttp.proxyHost=exaproxy.exa-corp.co.jp -Dhttp.proxyPort=8080 -Dhttps.proxyHost=exaproxy.exa-corp.co.jp -Dhttps.proxyPort=8080
+mvn package spring-boot:repackage -Dmaven.test.skip=true -Dhttp.proxyHost=exaproxy.exa-corp.co.jp -Dhttp.proxyPort=8080 -Dhttps.proxyHost=exaproxy.exa-corp.co.jp -Dhttps.proxyPort=8080

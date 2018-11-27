573a1feefb9507331391e9dbabe7e7381f53ea15
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-11-27 12:47:40 +0900

    r

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 ##export http_proxy=http://88429:Tomochi6@exaproxy.exa-corp.co.jp:8080
 ##export https_proxy=http://88429:Tomochi6@exaproxy.exa-corp.co.jp:8080
-export http_proxy=http://exaproxy.exa-corp.co.jp:8080
-export https_proxy=http://exaproxy.exa-corp.co.jp:8080
+#export http_proxy=http://exaproxy.exa-corp.co.jp:8080
+#export https_proxy=http://exaproxy.exa-corp.co.jp:8080
 ##sudo wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo -e HTTP_PROXY=exaproxy.exa-corp.co.jp:8080 -e HTTPS_PROXY=exaproxy.exa-corp.co.jp:8080
 #yum -y install wget
 #wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
 #yum -y install cf-cli
 ##export CF_STARTUP_TIMEOUT=30
 ##curl http://api.run.pivotal.io/
+printenv
 cf login -a api.run.pivotal.io -u tomoyuki-ito@exa-corp.co.jp -p P@ssw0rd
 ##cf login -a api.run.pivotal.io
 cd kantan_resource/kantanPro/target/

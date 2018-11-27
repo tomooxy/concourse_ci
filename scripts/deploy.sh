ffafd8a38f51a1433d046b689bbb2377d9e01c54
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-11-27 13:39:38 +0900

    r

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,12 @@
 #yum -y install wget
 #wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
 #yum -y install cf-cli
-##export CF_STARTUP_TIMEOUT=30
-curl https://api.run.pivotal.io/v2/info
+#export CF_STARTUP_TIMEOUT=30
+#curl https://api.run.pivotal.io/v2/info
+cf set-env http_proxy "exaproxy.exa-corp.co.jp:8080"
+cf set-env https_proxy "exaproxy.exa-corp.co.jp:8080"
 printenv
-cf login -a login.run.pivotal.io -u tomoyuki-ito@exa-corp.co.jp -p P@ssw0rd
+cf login -a api.run.pivotal.io -u tomoyuki-ito@exa-corp.co.jp -p P@ssw0rd
 ##cf login -a api.run.pivotal.io
 cd kantan_resource/kantanPro/target/
 cf push kantanPro-0.0.1-SNAPSHOT.jar -t 30

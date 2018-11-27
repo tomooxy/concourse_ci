766581016b1ed1ff4bb76c67d5df587301aa5339
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-11-27 11:36:31 +0900

    r

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@
 #export https_proxy=http://88429:Tomochi6@exaproxy.exa-corp.co.jp:8080
 export http_proxy=http://exaproxy.exa-corp.co.jp:8080
 export https_proxy=http://exaproxy.exa-corp.co.jp:8080
+sudo wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
+sudo yum -y install cf-cli
 #export CF_STARTUP_TIMEOUT=30
 #curl http://api.run.pivotal.io/
 cf login -a api.run.pivotal.io -u tomoyuki-ito@exa-corp.co.jp -p P@ssw0rd

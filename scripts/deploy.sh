3bd7ca6e22d4838e05babeab7e564fb01de35c8a
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-11-27 10:59:47 +0900

    e

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ export http_proxy=http://exaproxy.exa-corp.co.jp:8080
 export https_proxy=http://exaproxy.exa-corp.co.jp:8080
 #export CF_STARTUP_TIMEOUT=30
 #curl http://api.run.pivotal.io/
-cf -v
-cf login -a api.run.pivotal.io -u tomoyuki-ito@exa-corp.co.jp -p P@ssw0rd
+#cf login -a api.run.pivotal.io -u tomoyuki-ito@exa-corp.co.jp -p P@ssw0rd
+cf login -a api.run.pivotal.io
 cd kantan_resource/kantanPro/target/
 cf push kantanPro-0.0.1-SNAPSHOT.jar -t 30

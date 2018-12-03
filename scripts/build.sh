486d69fa7b91df35baca86e34d13d3288f0f008d
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-12-03 15:39:39 +0900

    r

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,4 @@
 cd kantan_resource/kantanPro
 #mvn package spring-boot:repackage
 mvn package spring-boot:repackage -Dmaven.test.skip=true -Dhttp.proxyHost=exaproxy.exa-corp.co.jp -Dhttp.proxyPort=8080 -Dhttps.proxyHost=exaproxy.exa-corp.co.jp -Dhttps.proxyPort=8080
+ls target

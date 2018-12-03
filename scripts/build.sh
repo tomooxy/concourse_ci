444c308b6daf876e50e17bdfbc055c6267309711
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-12-04 08:54:27 +0900

    r

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ cd kantan_resource/kantanPro
 #mvn package spring-boot:repackage
 mvn package spring-boot:repackage -Dmaven.test.skip=true -Dhttp.proxyHost=exaproxy.exa-corp.co.jp -Dhttp.proxyPort=8080 -Dhttps.proxyHost=exaproxy.exa-corp.co.jp -Dhttps.proxyPort=8080
 
-cd ~/
+cd ../../
 git clone https://github.com/tomooxy/concourse_test.git k_source
 cp kantan_resource/kantanPro/target/kantanPro-0.0.1-SNAPSHOT.jar k_source/kantanPro/target/
 cd k_source/kantanPro/target

55c34c891d8ce452c7043ab741bc55755f74ab58
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-12-04 13:42:31 +0900

    f

---
+++ scripts/build_centos.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+mkdir ~/.ssh
+touch ~/.ssh/github_id_rsa
+cp kantan_pipeline_resource/config ~/.ssh/
+echo "${MY_SECRET}" > ~/.ssh/github_id_rsa
+chmod 0600 ~/.ssh/github_id_rsa
+chmod 0600 ~/.ssh/config
+
+sudo yum install wget
+cd /etc/yum.repos.d/
+sudo wget http://wing-repo.net/wing/6/EL6.wing.repo
+sudo yum install git
+
+sudo yum -y install nc nmap
+
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+
+#export MAVEN_OPTS="-DsocksProxyHost=exaproxy.exa-corp.co.jp -DsocksProxyPort=8080"
+cd kantan_resource/kantanPro
+#mvn package spring-boot:repackage
+mvn package spring-boot:repackage -Dmaven.test.skip=true -Dhttp.proxyHost=exaproxy.exa-corp.co.jp -Dhttp.proxyPort=8080 -Dhttps.proxyHost=exaproxy.exa-corp.co.jp -Dhttps.proxyPort=8080
+
+cd ../../
+git clone git@github.com:tomooxy/concourse_test.git k_source
+cp kantan_resource/kantanPro/target/kantanPro-0.0.1-SNAPSHOT.jar k_source/kantanPro/target/
+cd k_source/kantanPro/target
+git add kantanPro-0.0.1-SNAPSHOT.jar
+
+#git add target/kantanPro-0.0.1-SNAPSHOT.jar
+git config --global user.name "conchan"
+git config --global user.email "tomoyuki-ito@exa-corp.co.jp"
+git commit -m "new build!"
+git push

8a2885200700d45977d1d5e52ba81f5023203027
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-12-04 14:52:37 +0900

    r

--- scripts/build_centos.sh
+++ scripts/build_centos.sh
@@ -8,7 +8,8 @@ chmod 0600 ~/.ssh/config
 
 yum -y install java-1.8.0-openjdk
 #readlink $(readlink $(which java))
-export JAVA_HOME= /usr/lib/jvm/java-1.8.0-openjdk-headless.x86_64 1:1.8.0.191.b12-1.el7_6
+ls /usr/lib/jvm/
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-headless.x86_64 1:1.8.0.191.b12-1.el7_6
 export PATH=$PATH:$JAVA_HOME/bin
 export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar 
 

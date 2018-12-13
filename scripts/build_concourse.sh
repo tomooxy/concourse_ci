071f3c77eba032b90fdeaaadf6d365749c047bcf
伊藤 智之 <88429@ad.exa-corp.co.jp>
2018-12-14 07:37:47 +0900

    docker hub利用

---
+++ scripts/build_concourse.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mkdir ~/.ssh
+touch ~/.ssh/github_id_rsa
+cp kantan_pipeline_resource/config ~/.ssh/
+echo "${MY_SECRET}" > ~/.ssh/github_id_rsa
+chmod 0600 ~/.ssh/github_id_rsa
+chmod 0600 ~/.ssh/config
+
+workpath=`pwd`
+
+cd $workpath/kantan_resource/kantanPro
+mvn package spring-boot:repackage -Dmaven.test.skip=true -Dhttp.proxyHost=exaproxy.exa-corp.co.jp -Dhttp.proxyPort=8080 -Dhttps.proxyHost=exaproxy.exa-corp.co.jp -Dhttps.proxyPort=8080
+
+cd ../../
+git clone git@github.com:tomooxy/concourse_test.git k_source
+cp kantan_resource/kantanPro/target/kantanPro-0.0.1-SNAPSHOT.jar k_source/kantanPro/target/
+cd k_source/kantanPro/target
+git add kantanPro-0.0.1-SNAPSHOT.jar
+
+#git add target/kantanPro-0.0.1-SNAPSHOT.jar
+git config --global user.name "conchan"
+git config --global user.email "tomoyuki-ito@exa-corp.co.jp"
+git commit -m "new build!"
+git push
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,6 +16,13 @@ touch key
 echo "${MY_SECRET}" > key
 chmod 0600 key
 scp -oStrictHostKeyChecking=no -i key kantan_resource/kantanPro/target/kantanPro-0.0.1-SNAPSHOT.jar vagrant@192.168.33.10:/home/vagrant/concourse/module/
+
+ssh -oStrictHostKeyChecking=no -i key vagrant@192.168.33.10 <<EOC
+cd /home/vagrant/concourse/module/
+cf login -a api.run.pivotal.io -u tomoyuki-ito@exa-corp.co.jp -p P@ssw0rd
+cf push kantanPro -p kantanPro-0.0.1-SNAPSHOT.jar -b https://github.com/cloudfoundry/java-buildpack.git
+EOC
+
 #cf login -a api.run.pivotal.io -u tomoyuki-ito@exa-corp.co.jp -p P@ssw0rd
 ##cf login -a api.run.pivotal.io
 #cd kantan_resource/kantanPro/target/
